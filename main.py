417578440297b8ad85fd2b3ee24ead6d2c1d900a
Kacper Kowalski <xperski@pm.me>
2025-04-02 22:35:53 +0200

    auto-commit

--- main.py
+++ main.py
@@ -43,39 +43,35 @@ def weblink(*args):
         wb.open_new(site)
 
 
-# ----------------------------------------------------- #
+if __name__ == "__main__":
+    w = Tk()
+    w.title('Mini MAL - PerSky <3')
+    w.resizable(False, False)
+    w.config(pady=30, padx=20, background='black')
+    w.minsize(300, 400)
+    w.attributes('-alpha', 1.0)
 
-w = Tk()
-w.title('Mini MAL - PerSky <3')
-w.resizable(False, False)
-w.config(pady=30, padx=20, background='black')
-w.minsize(300, 400)
-w.attributes('-alpha', 1.0)
+    image = (Image.open('v f x.png'))
+    resized_avatar = image.resize((100, 100))
+    img = ImageTk.PhotoImage(resized_avatar)
 
-image = (Image.open('v f x.png'))
-resized_avatar = image.resize((100, 100))
-img = ImageTk.PhotoImage(resized_avatar)
+    canvas = Canvas(width=100, height=100, highlightthickness=0)
+    canvas.create_image(50, 50, image=img)
+    canvas.place(relx=0.5, rely=0.02, anchor=N)
 
-canvas = Canvas(width=100, height=100, highlightthickness=0)
-canvas.create_image(50, 50, image=img)
-canvas.place(relx=0.5, rely=0.02, anchor=N)
+    search_text = Label(text='Search: ', bg='black', fg='purple')
+    search_text.place(relx=0.01, rely=0.45, anchor=W)
 
-search_text = Label(text='Search: ', bg='black', fg='purple')
-search_text.place(relx=0.01, rely=0.45, anchor=W)
+    sv = StringVar()
+    search_entry = Entry(width=30, textvariable=sv)
 
-sv = StringVar()
-search_entry = Entry(width=30, textvariable=sv)
-# sv.trace_add('write', search)
-search_entry.focus()
-search_entry.place(relx=0.22, rely=0.45, anchor=W)
+    search_entry.focus()
+    search_entry.place(relx=0.22, rely=0.45, anchor=W)
 
-# results = Label(text='', bg='black', fg='purple')
-# results.place(relx=0.5, rely=0.75, anchor=CENTER)
+    number = IntVar()
+    number_scale = Scale(w, bg='black', fg='white', length=250, from_=1, to=20, orient=HORIZONTAL, highlightthickness=0, activebackground='black', label='Number of results:', variable=number)
+    number_scale.place(relx=0.01, rely=0.65, anchor=W)
 
-number = IntVar()
-number_scale = Scale(w, bg='black', fg='white', length=250, from_=1, to=20, orient=HORIZONTAL, highlightthickness=0, activebackground='black', label='Number of results:', variable=number)
-number_scale.place(relx=0.01, rely=0.65, anchor=W)
+    w.bind('<Return>', search)
 
-w.bind('<Return>', search)
-
-w.mainloop()
+    w.mainloop()
